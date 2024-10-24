728dc92fa1ba6a4a187e92aedd5249aa6246717f
Francesc Sans <air.fsans@gmail.com>
2024-10-24 10:26:53 +0200

    Migrate FileMakerDialect to Hibernate 6

--- src/main/java/com/filemaker/hibernate/dialect/FileMakerDialect.java
+++ src/main/java/com/filemaker/hibernate/dialect/FileMakerDialect.java
@@ -2,179 +2,124 @@ package com.filemaker.hibernate.dialect;
 
 import java.sql.Types;
 
-import org.hibernate.LockMode;
+import org.hibernate.dialect.DatabaseVersion;
 import org.hibernate.dialect.Dialect;
-import org.hibernate.dialect.identity.IdentityColumnSupport;
 import org.hibernate.dialect.pagination.LimitHandler;
+import org.hibernate.engine.jdbc.dialect.spi.DialectResolutionInfo;
+import org.hibernate.query.spi.QueryEngine;
+import org.hibernate.sql.ast.SqlAstTranslatorFactory;
+import org.hibernate.type.descriptor.jdbc.JdbcType;
+import org.hibernate.type.descriptor.jdbc.spi.JdbcTypeRegistry;
 
-import com.filemaker.hibernate.dialect.identity.FileMakerIdentityColumnSupport;
 import com.filemaker.hibernate.dialect.pagination.FileMakerLimitHandler;
- 
-import static org.hibernate.type.SqlTypes.ROWID;
 
- @SuppressWarnings("deprecation")
- public class FileMakerDialect extends Dialect {
- 
-     public FileMakerDialect() {
- 
-         super();
- 
-         // database type mapping support ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-         registerColumnType(Types.VARCHAR, "VARCHAR");
-         registerColumnType(Types.LONGNVARCHAR, "VARCHAR");
- 
-         registerColumnType(Types.DOUBLE, "numeric");
-         registerColumnType(Types.DECIMAL, "decimal");
-         registerColumnType(Types.INTEGER, "int");
-         registerColumnType(Types.BOOLEAN, "int");
- 
-         registerColumnType(Types.DATE, "date");
-         registerColumnType(Types.TIME, "time");
-         registerColumnType(Types.TIMESTAMP, "timestamp");
- 
-         registerColumnType( Types.VARBINARY, "varbinary" );
-         registerColumnType( Types.LONGVARBINARY, "longvarbynary" );
-         registerColumnType(Types.BLOB, "blob");
- 
-         //registerColumnType(Types.CLOB, "clob");
- 
- 
-     }
- 
+public class FileMakerDialect extends Dialect {
 
+    public FileMakerDialect() {
+        this(DatabaseVersion.make(0, 0));
+    }
 
+    public FileMakerDialect(DatabaseVersion version) {
+        super(version);
+    }
 
-     // identity columns support ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-     @Override
-     public IdentityColumnSupport getIdentityColumnSupport() {
-         return new FileMakerIdentityColumnSupport();
-     }
- 
-     // limit/offset support ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-     @Override
-     public LimitHandler getLimitHandler() {
-         return new FileMakerLimitHandler();
-     }
- 
-     @Override
-     public boolean dropConstraints() {
-         return false;
-     }
- 
-     @Override
-     public boolean hasAlterTable() {
-         return false;
-     }
- 
-     @Override
-     public boolean supportsColumnCheck() {
-         return false;
-     }
- 
-     @Override
-     public boolean supportsCascadeDelete() {
-         return false;
-     }
- 
-     @Override
-     public boolean supportsLockTimeouts() {
-         return false;
-     }
- 
-     @Override
-     public boolean canCreateSchema() {
-         return false;
-     }
- 
- 
-     // Current Timestamp support ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-     @Override
-     public String getCurrentTimestampSQLFunctionName() {
-         return "current_timestamp";
-     }
- 
-     @Override
-     public boolean isCurrentTimestampSelectStringCallable() {
-         return false;
-     }
- 
-     @Override
-     public boolean supportsCurrentTimestampSelection() {
-         return true;
-     }
- 
- 
-     @Override
-     public boolean supportsOuterJoinForUpdate() {
-         return false;
-     }
- 
-     @Override
-     public boolean supportsTableCheck() {
-         return false;
-     }
- 
-     @Override
-     public boolean supportsUnionAll() {
-         return false;
-     }
- 
-     @Override
-     public boolean supportsUnique() {
-         return false;
-     }
- 
-     @Override
-     public String toBooleanValueString(boolean arg0) {
-         if (arg0) {
-             return "{b'true'}";
-         }
-         return "{b'false'}";
-     }
- 
-     // Locking support ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-     // TODO: check this
-     @Override
-     public String getForUpdateNowaitString() {
-         return "";
-     }
-     // TODO: check this
-     @Override
-     public String getForUpdateNowaitString(String aliases) {
-         return "";
-     }
- 
-     // TODO: check this
-     @Override
-     public String getForUpdateString() {
-         return " for update";
-     }
- 
-     // TODO: check this
-     @Override
-     public String getForUpdateString(LockMode lockMode) {
-         return "";
-     }
- 
-     // TODO: fix this
-     @Override
-     public String getForUpdateString(String aliases) {
-         return " for update of " + aliases;
-     }
- 
-     //Sequences support ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- 
-     @Override
-     public boolean supportsSequences() {
-         return false;
-     }
- 
-     @Override
-     public boolean supportsPooledSequences() {
-         return false;
-     }
- 
- 
- }
- 
- 
+    public FileMakerDialect(DialectResolutionInfo info) {
+        super(info);
+    }
+
+    @Override
+    public JdbcType resolveSqlTypeDescriptor(
+            String columnTypeName,
+            int jdbcTypeCode,
+            int precision,
+            int scale,
+            JdbcTypeRegistry jdbcTypeRegistry) {
+
+        switch (jdbcTypeCode) {
+            case Types.FLOAT:
+            case Types.TINYINT:
+            case Types.INTEGER:
+            case Types.DECIMAL:
+            case Types.BOOLEAN:
+                jdbcTypeCode = Types.DOUBLE;
+                break;
+            case Types.VARCHAR:
+            case Types.LONGNVARCHAR:
+                jdbcTypeCode = Types.VARCHAR;
+                break;
+        }
+        return super.resolveSqlTypeDescriptor(columnTypeName, jdbcTypeCode, precision, scale, jdbcTypeRegistry);
+    }
+
+    @Override
+    public LimitHandler getLimitHandler() {
+        return new FileMakerLimitHandler();
+    }
+
+    @Override
+    public boolean dropConstraints() {
+        return false;
+    }
+
+    @Override
+    public boolean hasAlterTable() {
+        return false;
+    }
+
+    @Override
+    public boolean supportsColumnCheck() {
+        return false;
+    }
+
+    @Override
+    public boolean supportsCascadeDelete() {
+        return false;
+    }
+
+    @Override
+    public boolean supportsLockTimeouts() {
+        return false;
+    }
+
+    @Override
+    public boolean canCreateSchema() {
+        return false;
+    }
+
+    @Override
+    public boolean isCurrentTimestampSelectStringCallable() {
+        return false;
+    }
+
+    @Override
+    public boolean supportsCurrentTimestampSelection() {
+        return true;
+    }
+
+    @Override
+    public boolean supportsOuterJoinForUpdate() {
+        return false;
+    }
+
+    @Override
+    public boolean supportsTableCheck() {
+        return false;
+    }
+
+    @Override
+    public boolean supportsUnionAll() {
+        return false;
+    }
+
+    // New method required for Hibernate 6
+    @Override
+    public SqlAstTranslatorFactory getSqlAstTranslatorFactory() {
+        return super.getSqlAstTranslatorFactory();
+    }
+
+    // New method required for Hibernate 6
+    @Override
+    public void initializeFunctionRegistry(QueryEngine queryEngine) {
+        super.initializeFunctionRegistry(queryEngine);
+    }
+}
