705a0b3f68b8c684196049525cd8c0c44b5abdb2
Francesc Sans <air.fsans@gmail.com>
2024-11-18 18:24:06 +0100

    updated deploy scripts

--- maven_deploy_dialect.sh
+++ maven_deploy_dialect.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
+# Check if version parameter is provided
+if [ $# -eq 0 ]; then
+    echo "Error: Version parameter is required"
+    echo "Usage: $0 <version>"
+    exit 1
+fi
+
+VERSION=$1
+
 mvn install:install-file \
--Dfile=./target/FileMakerDialect-21.0.2.jar \
+-Dfile=./target/FileMakerDialect-${VERSION}.jar \
 -DgroupId=com.filemaker.hibernate.dialect \
 -DartifactId=filemakerdialect \
--Dversion=21.0.2 \
+-Dversion=${VERSION} \
 -Dpackaging=jar \
 -DgeneratePom=true
--- maven_deploy_driver.sh
+++ maven_deploy_driver.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
+# Check if version parameter is provided
+if [ $# -eq 0 ]; then
+    echo "Error: Version parameter is required"
+    echo "Usage: $0 <version>"
+    exit 1
+fi
+
+VERSION=$1
+
 mvn install:install-file \
--Dfile=./src/main/resources/fmjdbc.jar \
--DgroupId=com.filemaker.jdbc.driver \
+-Dfile=./src/main/resources/fmjdbc.${VERSION}.jar \
+-DgroupId=com.filemaker.jdbc.Driver \
 -DartifactId=fmjdbc \
--Dversion=21.0.2 \
+-Dversion=${VERSION} \
 -Dpackaging=jar \
 -DgeneratePom=true
